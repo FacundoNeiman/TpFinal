b6b87fe7cb06aba686b90d4a21ca9d6b2d770a85
Facundo Neiman <115724830+FacundoNeiman@users.noreply.github.com>
2022-11-28 12:58:46 -0300

    feat add vehicle

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import mundial.*;
 import sistemaGarage.*;
+import sistemaGarage.vehiculos.Coche;
+import sistemaGarage.vehiculos.Vehiculo;
 
 public class Main {
     public static void main(String[] args) {
         Garage garage = new Garage();
         garage.abrirGarage(500,50);
+
+        Coche v1 = new Coche("MZK155", "Chevrolet", "Corsa Classic", 4, 50000, 4);
+        
         System.out.println(garage);
     }
 }
