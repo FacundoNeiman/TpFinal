bf88425a3545dccdd06fb332c83a65c674534405
Facundo Neiman <115724830+FacundoNeiman@users.noreply.github.com>
2022-11-28 12:32:25 -0300

    feat: add Main class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+import sistemaGarage.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Garage garage = new Garage();
+        garage.abrirGarage(500,50);
+        System.out.println(garage);
+    }
+}
