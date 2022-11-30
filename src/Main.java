569567107935a44012e632374962921f25ea15fd
Facundo Neiman <115724830+FacundoNeiman@users.noreply.github.com>
2022-11-30 20:12:19 -0300

    feat: add all teams in Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 import mundial.*;
@@ -14,10 +15,39 @@ public class Main {
         
         // System.out.println(garage);
 
+        // Equipo qatar = new Equipo("Qatar");
+        // Equipo paisesBajos = new Equipo("Países Bajos");
+        // Equipo ecuador = new Equipo("Ecuador");
+        // Equipo senegal = new Equipo("Senegal");
+        // Equipo inglaterra = new Equipo("Inglaterra");
+        // Equipo estadosUnidos = new Equipo("Estados Unidos");
+        // Equipo iran = new Equipo("Irán");
+        // Equipo gales = new Equipo("Gales");
         Equipo argentina = new Equipo("Argentina");
         Equipo polonia = new Equipo("Polonia");
         Equipo arabia = new Equipo("Arabia Saudita");
         Equipo mexico = new Equipo("México");
+        Equipo francia = new Equipo("Francia");
+        Equipo dinamarca = new Equipo("Dinamarca");
+        Equipo australia = new Equipo("Australia");
+        Equipo tunez = new Equipo("Tunez");
+        // Equipo espana = new Equipo("España");
+        // Equipo japon = new Equipo("Japon");
+        // Equipo costaRica = new Equipo("Costa Rica");
+        // Equipo alemania = new Equipo("Alemania");
+        // Equipo croacia = new Equipo("Croacia");
+        // Equipo marruecos = new Equipo("Marruecos");
+        // Equipo belgica = new Equipo("Belgica");
+        // Equipo canada = new Equipo("Canada");
+        // Equipo brasil = new Equipo("Brasil");
+        // Equipo suiza = new Equipo("Suiza");
+        // Equipo camerun = new Equipo("Camerún");
+        // Equipo serbian = new Equipo("Serbia");
+        // Equipo portugal = new Equipo("Portugal");
+        // Equipo ghana = new Equipo("Ghana");
+        // Equipo corea = new Equipo("Corea");
+        // Equipo uruguay = new Equipo("Uruguay");
+        
         Grupo grupoC = new Grupo();
         Date fecha = new Date(2022, 11, 20);
         grupoC.addPartido(new Partido(fecha, argentina, arabia, new Resultado(1,2)));
@@ -27,12 +57,19 @@ public class Main {
         grupoC.addPartido(new Partido(fecha,argentina,polonia,new Resultado(2,0)));
         grupoC.addPartido(new Partido(fecha,arabia,mexico,new Resultado(1,2)));
 
-        System.out.println(grupoC.getEquiposQueAvanzan());
+        Grupo grupoD = new Grupo();
+        grupoD.addPartido(new Partido(fecha, dinamarca, tunez, new Resultado(0,0)));
+        grupoD.addPartido(new Partido(fecha, francia, australia, new Resultado(4,1)));
+        grupoD.addPartido(new Partido(fecha, tunez, australia, new Resultado(0,1)));
+        grupoD.addPartido(new Partido(fecha, francia, dinamarca, new Resultado(2,1)));
+        grupoD.addPartido(new Partido(fecha, tunez, francia, new Resultado(1,0)));
+        grupoD.addPartido(new Partido(fecha, australia, dinamarca, new Resultado(1,0)));
 
-        Llave op1 = new Llave("Octavos de Final");
-        op1.addPartido(new Partido(fecha, argentina, mexico, new Resultado(1,0)));
-        for (Equipo e : op1.getEquiposQueAvanzan()) {
-            System.out.println(e.getNombre());
-        }
+        ArrayList<Equipo> clasificadosGrupoC =  grupoC.getEquiposQueAvanzan();
+        ArrayList<Equipo> clasificadosGrupoD =  grupoD.getEquiposQueAvanzan();
+
+        Llave llaveA = new Llave("Semifinales");
+        llaveA.addPartido(new Partido(fecha, clasificadosGrupoC.get(0), clasificadosGrupoD.get(1), new Resultado(2,1)));
+        llaveA.addPartido(new Partido(fecha, clasificadosGrupoC.get(1), clasificadosGrupoD.get(0), new Resultado(0,3)));
     }
 }
