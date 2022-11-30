6b5519aca6504d9f5d2e21559b9a1f25e55c72ed
Facundo Neiman <115724830+FacundoNeiman@users.noreply.github.com>
2022-11-30 19:26:24 -0300

    feat: equiposQueAvanzan in Grupo

--- src/mundial/Grupo.java
+++ src/mundial/Grupo.java
@@ -17,10 +17,19 @@ public class Grupo extends EtapaMundial{
     public ArrayList<Equipo> getEquiposQueAvanzan() {
         //TODO
         ArrayList<Equipo> equiposQueAvanzan = new ArrayList<>();
+        Equipo e1 = new Equipo("");
+        Equipo e2 = new Equipo("");
         for (Equipo equipo : super.getEquipos()) {
-            
+            if(equipo.getPuntaje() > e1.getPuntaje()){
+                e2 = e1;
+                e1 = equipo;
+            }
+            else if (equipo.getPuntaje() > e2.getPuntaje()) {
+                e2 = equipo;
+            }
         }
-
+        equiposQueAvanzan.add(e1);
+        equiposQueAvanzan.add(e2);
         return equiposQueAvanzan;
     }
 
@@ -29,6 +38,7 @@ public class Grupo extends EtapaMundial{
         super.getPartidos().add(partido);
         partido.getLocal().addPartido(partido);
         partido.getVisitante().addPartido(partido);
-        
+        super.addEquipo(partido.getLocal());
+        super.addEquipo(partido.getVisitante());
     }
 }
